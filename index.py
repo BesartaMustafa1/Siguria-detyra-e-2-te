3d91aad8c88a6a7a6262978918f9deacf7eeafc4
Auronmussliu1 <auron.musliu@student.uni-pr.edu>
2024-04-19 22:54:09 +0200

    keystream generation complete

--- index.py
+++ index.py
@@ -1,6 +1,16 @@
 import hashlib #libraria e marr ne baze te ushtrimeve ne klase
 print("Goodbye, World!")
 #fillimisht po e gjenerojme nje key stream me nje string seed
-def generate_keystream(seed,message_length):
-    if isinstance(seed,str):
-        seed=seed.encode('utf-8') #duhet me bo utf-8 qe me konvertu seeds ne byte
+def generate_keystream(seed, message_length):
+
+    if isinstance(seed, str):
+        seed = seed.encode('utf-8') #duhet me bo utf-8 qe me konvertu seeds ne byte
+    seed = int.from_bytes(hashlib.sha256(seed).digest(), byteorder='big')
+    keystream = []
+    for i in range(message_length):
+        seed ^= seed<<13 
+        seed ^= seed>>17 
+        seed ^= seed<<5 
+        num = seed & 0x7fffffff 
+        keystream.append(num % 256)
+    return keystream    
