4609c91998472dc3e71bf98a86ffaa8c564da0f3
Auronmussliu1 <auron.musliu@student.uni-pr.edu>
2024-04-19 23:04:32 +0200

    fillimi i funksionit otp-encypt dhe komentimi i keystream

--- index.py
+++ index.py
@@ -7,10 +7,17 @@ def generate_keystream(seed, message_length):
         seed = seed.encode('utf-8') #duhet me bo utf-8 qe me konvertu seeds ne byte
     seed = int.from_bytes(hashlib.sha256(seed).digest(), byteorder='big')
     keystream = []
+    #Per keystream-in e perdorim XOR-shifts
     for i in range(message_length):
-        seed ^= seed<<13 
-        seed ^= seed>>17 
-        seed ^= seed<<5 
-        num = seed & 0x7fffffff 
-        keystream.append(num % 256)
+        seed ^= seed<<13 #Kjo ben shift vleren "seed" majtas per 13 pozisione, pastaj ben kete vler XOR me vleren e vjeter "seed"
+        seed ^= seed>>17 #Kjo ben shift vleren "seed" djathas per 17 pozisione dhe pastaj ben kete vler XOR me vleren e vjeter "seed"
+        seed ^= seed<<5 #Kjo ben shift vleren "seed" majtas per 5 pozisione dhe pastaj ben kete vler XOR me vleren e vjeter "seed"
+        num = seed & 0x7fffffff #Ketu vetem 31 bitet me te majt te vleres "seed" jane perdorur qe ne fund numri te jete pozitiv
+        keystream.append(num % 256) #Ketu sigurohet qe vlera perfundimtare te jete ne mes 0 dhe 255(e moduluar me 255), kete eben duke e zbritur numrin e percaktuar me 256
     return keystream    
+#Ne vazhdim funksioni one time pad encryption (otp_encrypt)
+
+def etp_encrypt(message, seed):
+    message_bytes = message.encode()
+    keystream = generate_keystream(seed, len(message_bytes))
+    encrypted_message = bytearray()
