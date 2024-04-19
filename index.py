c9d7e766a7ee95a0e46188737a6f588ac749b9e9
Besarta Mustafa <besartamustafa49@gmail.com>
2024-04-19 14:35:28 +0200

    Gjenerimi i celesit ??

--- index.py
+++ index.py
@@ -3,5 +3,4 @@ print("Goodbye, World!")
 #fillimisht po e gjenerojme nje key stream me nje string seed
 def generate_keystream(seed,message_length):
     if isinstance(seed,str):
-        seed=seed.encode('utf-8')
-        
+        seed=seed.encode(seed)
