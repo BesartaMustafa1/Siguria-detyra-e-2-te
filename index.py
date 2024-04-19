e29482f65684be1560310873af2523c8893dc19f
Besarta Mustafa <besartamustafa49@gmail.com>
2024-04-19 14:35:08 +0200

    Gjenerimi i celesit ??

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
 import hashlib #libraria e marr ne baze te ushtrimeve ne klase
 print("Goodbye, World!")
+#fillimisht po e gjenerojme nje key stream me nje string seed
+def generate_keystream(seed,message_length):
+    if isinstance(seed,str):
+        seed=seed.encode('utf-8')
+        
